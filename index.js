2348384338800367e2c86a484738d8fb19ed69c5
Demo-159 <davidmonrroy7@gmail.com>
2025-06-18 00:21:05 -0400

    Actualizar index.js

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ const dataset = {
         logo: "https://peach.blender.org/wp-content/uploads/title_anouncement.jpg",
         runtime: "10 min",
         // Stream HLS directo - ACTUALIZADO CON TU SERVIDOR
-        url: "https://video.gumlet.io/684cd82890b0148cd24b3fab/684cdcc9c4269590ab78ef00/main.m3u8",
+        url: "https://pixeldrain.com/api/file/uVTP2kza",
         title: "HLS Stream",
         // Configuraciones adicionales para mejorar compatibilidad
         behaviorHints: {
